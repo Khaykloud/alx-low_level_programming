080bb20ed6cab3d3458114649bb4579cbd6de33d
Kayode Osobu <youngkhays@gmail.com>
2023-02-17 02:48:42 -0800

    vi 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 
 /**
 
- * main -  prints "Programming is like building a multilingual
- * puzzle, followed by a newline.
- * Return: Always 0.
+ * main -  Entry point
+
+ *
+
+ * Return: Always 0 (Success)
  */
 
 int main(void)
